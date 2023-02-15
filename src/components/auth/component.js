e20949505a6bdf724bcf4ae83ac1564e8dd41961
Eric Thuaud <eric.thuaud@insee.fr>
2023-02-15 16:22:58 +0100

    fix - variabilize identity provider

--- src/components/auth/component.js
+++ src/components/auth/component.js
@@ -1,11 +1,11 @@
 /* eslint-disable react/prop-types */
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import AppContainer from 'containers/app';
-import Keycloak from 'keycloak-js';
+import AppContainer from "containers/app";
+import Keycloak from "keycloak-js";
 
-const KEYCLOAK = 'keycloak';
-const NO_AUTH = 'anonymous';
+const KEYCLOAK = "keycloak";
+const NO_AUTH = "anonymous";
 const confKeycloak = `${window.location.origin}/keycloak.json`;
 const apiConfigPath = `${window.location.origin}/configuration.json`;
 
@@ -21,34 +21,39 @@ const Auth = ({
   const [token, setToken] = useState(null);
   const [kc, setKc] = useState(null);
   const [error, setError] = useState(null);
+  const [identityProvider, setIdentityProvider] = useState(null);
 
   useEffect(() => {
     if (!authType) {
       fetch(apiConfigPath)
-        .then(response => response.json())
-        .then(data => {
+        .then((response) => response.json())
+        .then((data) => {
           saveUrlBackEnd(data.urlBackEnd);
           saveUrlSiteMirroir(data.urlSiteMirroir);
           saveUrlColemanPromotion(data.urlColemanPromotion);
           setAuthType(data.authType);
+          setIdentityProvider(data.identityProvider);
         });
     }
-    if (authType === KEYCLOAK) {
+    if (authType === KEYCLOAK && identityProvider) {
       const keycloakTemp = new Keycloak(confKeycloak);
       const kcLogin = keycloakTemp.login;
-      keycloakTemp.login = options => {
+      keycloakTemp.login = (options) => {
         const options2 = options;
-        options2.idpHint = 'sso-insee';
+        options2.idpHint = identityProvider;
         kcLogin(options2);
       };
-      keycloakTemp.redirectUri = window.location.href.replace(window.location.search, '');
+      keycloakTemp.redirectUri = window.location.href.replace(
+        window.location.search,
+        ""
+      );
       setKc(keycloakTemp);
     }
-  }, [authType]);
+  }, [authType, identityProvider]);
 
   const refreshToken = () => {
     kc.updateToken(30)
-      .then(isUpdated => {
+      .then((isUpdated) => {
         if (isUpdated) {
           setToken(kc.token);
           refreshKeycloakToken(kc);
@@ -65,7 +70,7 @@ const Auth = ({
 
   useEffect(() => {
     if (authType === KEYCLOAK && !!kc) {
-      kc.init({ onLoad: 'login-required' })
+      kc.init({ onLoad: "login-required" })
         .then(() => {
           setToken(kc.token);
           saveKeycloakToken(kc);
@@ -79,7 +84,9 @@ const Auth = ({
 
   return (
     <>
-      {!error && (!authType || (authType === KEYCLOAK && !token)) && <div>Loading ...</div>}
+      {!error && (!authType || (authType === KEYCLOAK && !token)) && (
+        <div>Loading ...</div>
+      )}
       {!error && authType === KEYCLOAK && token && <AppContainer />}
       {!error && authType === NO_AUTH && <AppContainer />}
       {error && <div>Erreur inconnue - probléme authentification !</div>}
