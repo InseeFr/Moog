f1052bb228dca0bd2b468fd8b13b7f5bc9c12855
Eric Thuaud <eric.thuaud@insee.fr>
2023-04-21 17:33:55 +0200

    fix - finalize feat

--- src/utils/roles.js
+++ src/utils/roles.js
@@ -1,15 +1,13 @@
 import store from "store/configure-store";
 
 export default function estGestionnaire(kcRoles) {
-  console.log(kcRoles);
-  console.log(store.getState().roles.roleGestionnaire);
   const test =
     Array.isArray(kcRoles) &&
     (store.getState().roles.roleGestionnaire.some((r) => kcRoles.includes(r)) ||
       store
         .getState()
         .roles.roleAdministrateur.some((r) => kcRoles.includes(r)));
-  console.log(test);
+
   return test;
 }
 
