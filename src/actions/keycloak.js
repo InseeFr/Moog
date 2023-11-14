439787c58efed7f6b87cf907ad7eba49847817bf
Eric Thuaud <58465319+EricThuaud@users.noreply.github.com>
2023-11-14 17:16:55 +0100

    Add logout button (#26)
    * chore - modify scripts for local exec with node above 16
    
    * fix - finalize PR
    
    * fix - remove build options script

--- src/actions/keycloak.js
+++ src/actions/keycloak.js
@@ -2,8 +2,17 @@ import {
   SAVE_KEYCLOAK_TOKEN,
   REFRESH_KEYCLOAK_TOKEN,
   SAVE_NO_AUTH_LOGIN,
-} from './constants/keycloak';
+} from "./constants/keycloak";
 
-export const saveKeycloakToken = kc => ({ type: SAVE_KEYCLOAK_TOKEN, payload: kc });
-export const saveNoAuthLogin = () => ({ type: SAVE_NO_AUTH_LOGIN, payload: null });
-export const refreshKeycloakToken = kc => ({ type: REFRESH_KEYCLOAK_TOKEN, payload: kc });
+export const saveKeycloakToken = (kc) => ({
+  type: SAVE_KEYCLOAK_TOKEN,
+  payload: kc,
+});
+export const saveNoAuthLogin = () => ({
+  type: SAVE_NO_AUTH_LOGIN,
+  payload: null,
+});
+export const refreshKeycloakToken = (kc) => ({
+  type: REFRESH_KEYCLOAK_TOKEN,
+  payload: kc,
+});
